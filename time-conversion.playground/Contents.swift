201e91b73e034510a4cafc37a44e027377ecee30
Mike Baldwin <mikembaldwin@gmail.com>
2018-08-09 16:37:14 -0700

    Find working solution to exercise

---
+++ time-conversion.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Time Conversion: https://www.hackerrank.com/challenges/time-conversion/problem
+
+func timeConversion(s: String) -> String {
+    var twelveHourTime = s // Because I like descriptive variable names
+    
+    let isAfterNoon = twelveHourTime.hasSuffix("PM")
+    twelveHourTime.removeLast(2)
+
+    let components = twelveHourTime.split(separator: ":")
+    var hourAsInt = Int(components[0])!
+    
+    if isAfterNoon {
+        hourAsInt += 12
+    }
+    
+    let newHour = (hourAsInt < 10) ? "0" + String(hourAsInt) : String(hourAsInt)
+    
+    return "\(newHour):\(components[1]):\(components[2])"
+}
+
+timeConversion(s: "07:05:45PM")
