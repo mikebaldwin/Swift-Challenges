ae3136c3bab3863b0ca0f2e8108813abdbefcba4
Mike Baldwin <mikembaldwin@gmail.com>
2018-08-10 06:57:34 -0700

    Write working solution to exercise

--- GradingStudents.playground/Contents.swift
+++ GradingStudents.playground/Contents.swift
@@ -1,18 +1,35 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
+import Foundation
 
 // Grading Students: https://www.hackerrank.com/challenges/grading/problem
 
 func gradingStudents(grades: [Int]) -> [Int] {
     var roundedGrades: [Int] = []
-    
-    // For each grade over 40, find the next multiple of 5.
-    
-    // If the difference is less than 3, round up
-    
+    // For each grade over 37, find the next multiple of 5.
+    for grade in grades {
+        // Don't round anything less than 38, since it will still be a failing grade
+        if grade < 38 {
+            roundedGrades.append(grade)
+            continue
+        }
+        // Find next multiple of 5
+        if grade % 5 != 0 {
+            var nextMultipleOfFive = grade
+            while nextMultipleOfFive % 5 != 0 {
+                nextMultipleOfFive += 1
+            }
+            if nextMultipleOfFive - grade < 3 {
+                roundedGrades.append(nextMultipleOfFive)
+            } else {
+                roundedGrades.append(grade)
+            }
+        } else {
+            roundedGrades.append(grade)
+        }
+    }
     return roundedGrades
 }
 
-let sampleInput = [73, 67, 38, 33]
+let sampleInput = [73, 67, 38, 33, 100, 1, 0]
 gradingStudents(grades: sampleInput)
