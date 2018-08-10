6f41b6c59fe722691adeae090482af1168cf91e5
Mike Baldwin <mikembaldwin@gmail.com>
2018-08-10 06:33:45 -0700

    Set up exercise

---
+++ GradingStudents.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Grading Students: https://www.hackerrank.com/challenges/grading/problem
+
+func gradingStudents(grades: [Int]) -> [Int] {
+    var roundedGrades: [Int] = []
+    
+    // For each grade over 40, find the next multiple of 5.
+    
+    // If the difference is less than 3, round up
+    
+    return roundedGrades
+}
+
+let sampleInput = [73, 67, 38, 33]
+gradingStudents(grades: sampleInput)
